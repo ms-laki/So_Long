2cd4c62fe9bb30d385add9b270b3a66d9499a6b8
Lucky <87255552+ms-laki@users.noreply.github.com>
2022-10-28 19:09:46 +1030

    Added final So_Long project

---
+++ headers/so_long.h
@@ -0,0 +1,56 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   so_long.h                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: lgiirre <marvin@42.fr>                     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/10/26 20:09:05 by lgiirre           #+#    #+#             */
+/*   Updated: 2022/10/26 20:09:13 by lgiirre          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef SO_LONG_H
+# define SO_LONG_H
+
+# include <unistd.h>
+# include <stdio.h>
+# include <fcntl.h>
+# include <errno.h>
+# include <string.h>
+# include "get_next_line.h"
+# include "../minilibx/mlx.h"
+
+typedef struct t_start
+{
+	int		fd;
+	int		heightmap;
+	int		widthmap;
+	int		playercount;
+	int		columncount;
+	int		exitcount;
+	int		x_axis;
+	int		y_axis;
+	int		counter;
+	int		collectables;
+
+	char	**map;
+
+	void	*floor;
+	void	*barrier;
+	void	*player;
+	void	*exit;
+	void	*collectable;
+	void	*mlxpointer;
+	void	*winpointer;
+
+}	t_complete;
+
+int		exit_point(t_complete *game);
+int		map_reading(t_complete *game, char **argv);
+int		controls_working(int command, t_complete *game);
+void	adding_in_graphics(t_complete *game);
+void	place_images_in_game(t_complete *game);
+void	check_errors(t_complete *game);
+
+#endif
